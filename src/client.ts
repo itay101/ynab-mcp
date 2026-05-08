67de924762e2b8ed1d84a1a2a7c610b8dbfa90df
Itay Roisman <itay101@gmail.com>
2026-05-08 21:25:22 +0300

    refactor: replace getClient singleton with token parameter

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,5 @@
 import * as ynab from "ynab";
 
-let client: ynab.API | null = null;
-
-export function getClient(): ynab.API {
-  if (!client) {
-    const token = process.env.YNAB_API_TOKEN;
-    if (!token) {
-      console.error("Error: YNAB_API_TOKEN environment variable is not set.");
-      process.exit(1);
-    }
-    client = new ynab.API(token);
-  }
-  return client;
+export function getClient(token: string): ynab.API {
+  return new ynab.API(token);
 }
